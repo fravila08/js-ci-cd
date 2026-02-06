91cdfe8ff2ce0132b52f724d80c820fad01342ed
fravila08 <fr4v1l4@gmail.com>
2026-02-06 09:27:17 -0800

    workflow for test suite added

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function factorial(num) {
   let product = 1;
 
-  for (let i = num; i > 0; i--) {
-    product = product * i;
-  }
+  // for (let i = num; i > 0; i--) {
+  //   product = product * i;
+  // }
 
   return product;
 }
